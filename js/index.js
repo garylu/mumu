e60a3505dfab43646ac681c139d8d058c26fa21f
Wykay-z <wykayhk@gmail.com>
2015-10-11 13:28:48 +0800

    1011修改
    样式键盘、颜文字键盘；左侧边栏定位问题；a标签改为div；

--- js/index.js
+++ js/index.js
@@ -1,7 +1,27 @@
 $(function(){
 
-    
-
+ 
+   // 样式选择、颜文字选择
+	$("#chat-box-input").click(function(){
+		if ($('#keyboard-face').is(':visible')) {
+			$('#keyboard-face').hide();
+		}
+		if ($('#keyboard-color').is(':visible')) {
+			$('#keyboard-color').hide();
+		}
+	});
+	$("#color-btn").click(function(){
+		if ($('#keyboard-face').is(':visible')) {
+			$('#keyboard-face').hide();
+		}
+		$("#keyboard-color").fadeToggle("fast");
+	});
+	$("#face-btn").click(function(){
+		if ($('#keyboard-color').is(':visible')) {
+			$('#keyboard-color').hide();
+		}
+		$("#keyboard-face").fadeToggle("fast");
+	});
 
 
 
--- js/socket.js
+++ js/socket.js
@@ -50,8 +50,8 @@ bindEvent(document.body, 'keydown', function(e) {
 });
 
 styleBtn.onclick = function(){
-  changeStyle('dialog-style1');
-  setCookie('userStyle','dialog-style1',365);
+  changeStyle('color1');
+  setCookie('userStyle','color1',365);
 };
 
 function main() {
@@ -281,7 +281,7 @@ function getLog(callback) {
 function showLog(msg, data, isBefore) {
   if (data) {
     // console.log(msg, data);
-    msg = msg + '<li class="dialog-style6 mine-reply"><p>' + encodeHTML(JSON.stringify(data)) + '</p></li>';
+    msg = msg + '<li class="color6 mine-reply"><p>' + encodeHTML(JSON.stringify(data)) + '</p></li>';
   }
   var p = document.createElement('p');
   p.innerHTML = msg;
@@ -346,7 +346,7 @@ function checkCookie(){
   if (userStyle != null && userStyle != "") {
     changeStyle(userStyle); 
   } else {
-    userStyle = "dialog-style6";
+    userStyle = "color6";
     if (userStyle!=null && userStyle!="")
     {
       setCookie('userStyle',userStyle,365);
@@ -384,6 +384,7 @@ function getHeight(){
   var height_screen = screen.height;
   var height = height_screen - 90;
   document.getElementById("dialog-list").style.height = height+"px";
+  document.getElementById("room-list").style.height = height+"px";
 }
 
 // 生成uuid
